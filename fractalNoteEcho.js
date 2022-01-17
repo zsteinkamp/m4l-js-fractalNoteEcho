da6656e47b886644730edd8f7ae905d63f33388f
Zack Steinkamp <zack@steinkamp.us>
2022-01-16 17:35:52 -0800

    clean up; make more live-friendly

--- fractalNoteEcho.js
+++ fractalNoteEcho.js
@@ -1,12 +1,96 @@
 autowatch=1;
 inlets=12;
-outlets=3;
+outlets=5;
 
 // set up sketch canvas
 sketch.default2d();
 sketch.glloadidentity();
 
-var utils = require("utils.js");
+var utils = {
+  // scale the values in a numeric array to the bounds specified in newMin, newMax
+  scale: function(array, newMin, newMax) {
+    // get range
+    var min = null
+    var max = null;
+    for (var i = 0; i < array.length; i++) {
+      if (min === null || array[i] < min) { min = array[i]; }
+      if (max === null || array[i] > max) { max = array[i]; }
+    }
+    var range = max - min;
+
+    var newRange = newMax - newMin;
+
+    var coeff = range ? newRange / parseFloat(range) : 0.0;
+
+    var offset = newMin - (min * coeff);
+
+    var returnArray = [];
+    for (var i = 0; i < array.length; i++) {
+      returnArray.push(array[i] * coeff + offset);
+    }
+
+    //exports.log({
+    //    min: min,
+    //    max: max,
+    //    range: range,
+    //    newRange: newRange,
+    //    coeff: coeff,
+    //    offset: offset,
+    //    return: returnArray
+    //});
+
+    return returnArray;
+  },
+
+  HSLToRGB: function(h,s,l) {
+    //exports.log({ h: h, s: s, l: l });
+
+    var c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c/2,
+        r = 0,
+        g = 0,
+        b = 0;
+    if (0 <= h && h < 60) {
+      r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+      r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+      r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+      r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+      r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+      r = c; g = 0; b = x;
+    }
+    return {
+      r: r + m,
+      g: g + m,
+      b: b + m
+    };
+  },
+
+  log: function() {
+    for(var i=0,len=arguments.length; i<len; i++) {
+      var message = arguments[i];
+      if(message && message.toString) {
+        var s = message.toString();
+        if(s.indexOf("[object ") >= 0) {
+          s = JSON.stringify(message);
+        }
+        post(s);
+      }
+      else if(message === null) {
+        post("<null>");
+      }
+      else {
+        post(message);
+      }
+    }
+    post("\n");
+  }
+};
 
 // inlets
 var INLET_NOTE = 0;
@@ -26,6 +110,8 @@ var INLET_DUR_DECAY = 11;
 var OUTLET_NOTE = 0;
 var OUTLET_VELOCITY = 1;
 var OUTLET_DURATION = 2;
+var OUTLET_TOTAL_NOTES = 3;
+var OUTLET_TOTAL_DURATION = 4;
 
 // state arrays
 var pattern = []; // base tap pattern
@@ -235,13 +321,17 @@ function draw() {
   }
 
   // Add some informational text
-  if (noteRepeats.length > 0) {
-    sketch.moveto(xMin - baseDia, yMax);
-    sketch.textalign("left");
-    var lastTap = noteRepeats[noteRepeats.length - 1];
-    sketch.glcolor(1,1,1,1); // white text
-    sketch.text(noteRepeats.length + " Notes // Total " + parseInt(lastTap.ms + lastTap.duration)/1000 + " seconds");
-  }
+  var lastTap = noteRepeats[noteRepeats.length - 1];
+  outlet(OUTLET_TOTAL_NOTES, noteRepeats.length);
+  outlet(OUTLET_TOTAL_DURATION, parseInt(lastTap.ms + lastTap.duration)/1000);
+
+  //if (noteRepeats.length > 0) {
+  //  sketch.moveto(xMin - baseDia, yMax);
+  //  sketch.textalign("left");
+  //  
+  //  sketch.glcolor(1,1,1,1); // white text
+  //  sketch.text(noteRepeats.length + " Notes // Total " + parseInt(lastTap.ms + lastTap.duration)/1000 + " seconds");
+  //}
 }
 
 // Utility to scale a value from one range to another
