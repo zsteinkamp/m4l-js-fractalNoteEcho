ef9307148eaf10e515dc91335e879abfa576cfd7
Zack Steinkamp <zack@steinkamp.us>
2022-06-07 21:03:43 -0700

    improve look and feel

--- fractalNoteEcho.js
+++ fractalNoteEcho.js
@@ -256,7 +256,7 @@ function handleMessage(i) {
 
 function draw() {
   // clear the jsui area
-  sketch.glclearcolor(1.0, 1.0, 1.0, 0.0); // transparent
+  sketch.glclearcolor(0.15, 0.15, 0.15, 1); // transparent
   sketch.glclear();
 
   var lastRepeat = vizRepeats[vizRepeats.length - 1];
